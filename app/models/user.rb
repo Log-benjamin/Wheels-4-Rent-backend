c324d3e7ccaaf245b01bc58fdd2235541836c063
binyamT <binyampowerhc@gmail.com>
2023-11-09 23:48:21 +0300

    create the admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password_digest, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+    admin=User.create({ name: "admin", email: 'admin@wheels4rent.com', password_digest: '123456789', is_admin: true })
+
+    if admin.valid?
+        admin.save()
+
+    elsif admin.errors.any?
+        admin.errors.full_messages.each do |msg|
+            puts msg
+        end
+    else
+        puts "****NOT VALID****"
+    end
