06a6d0eb479f76c46a17a64ad1df97743192624e
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-09 18:54:45 +0300

    Add validations to the models for Car, Reservation, and User.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy
+
+  validates :name, presence: true
+  validates :model, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validates :price_per_day, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :reservations, foreign_key: 'user_id', dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
