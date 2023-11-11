d319813d8674bcf41a206f44b955c0d0bb6f0778
binyamT <binyampowerhc@gmail.com>
2023-11-11 06:18:00 +0300

    create registration and token controller

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,13 @@
+class RegistrationController < ApplicationController
+  def create
+    user = User.new(params.permit(:name, :email, :password))
+    if user.save
+      render json: {
+        status: :created,
+        user:
+      }
+    else
+      :bad_request
+    end
+  end
+end
