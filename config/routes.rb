aad5c381bec4136d8da3c12535ed604ce4673c16
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-13 19:34:46 +0300

    Update the routes with the api path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :registration
-  resources :token, only: [:create]
+  namespace :api do
+    namespace :v1 do
+      resources :registration
+      resources :token, only: [:create]
+    end
+  end
 end
