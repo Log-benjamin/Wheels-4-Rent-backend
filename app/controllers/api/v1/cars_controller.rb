37573688fd6c818f428feb765e45dd8e7a22d36a
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-15 18:48:56 +0300

    Update CarsController for CRUD operations

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -11,9 +11,24 @@ class Api::V1::CarsController < ApplicationController
     render json: @car
   end
 
+  # POST /cars
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      render json: { status: 200, car: @car }
+    else
+      render json: { status: 404, errorMessage: @car.errors.full_messages.to_sentence }
+    end
+  end
+
   private
 
   def set_car
     @car = Car.find(params[:id])
+  end
+
+  def car_parms
+    params.require(:car).permit(:name, :model, :description, :image, :price_per_day)
   end
 end
