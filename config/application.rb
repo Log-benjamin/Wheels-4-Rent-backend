8d63ca3ce14c0d105a39448aedb1f3c423bf0cfc
binyamT <binyampowerhc@gmail.com>
2023-11-15 03:23:44 +0300

    updatethe configration to render json file properly

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Wheels4RentBackend
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.action_controller.default_protect_from_forgery = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
