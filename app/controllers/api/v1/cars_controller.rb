ca1e4e88bce3134580e9c46bd1bd094c4b8c8042
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-16 17:16:09 +0300

    update the befor action call

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CarsController < ApplicationController
-  before_action :set_car, only: %i[show]
+  before_action :set_car, only: %i[show destroy]
   # GET /cars
   def index
     @cars = Car.all
