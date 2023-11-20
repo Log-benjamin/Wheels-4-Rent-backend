4a5e304c2e4eb02943bb14d7460dffda2bddd5dc
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-20 13:22:31 +0300

    Fix car model issues

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,6 +5,6 @@ class Car < ApplicationRecord
   validates :model, presence: true
   validates :description, presence: true
   validates :image, presence: true
-  validates :seating_capacity: true, numericality: { greater_than: 0 }
+  validates :seating_capacity, presence: true, numericality: { greater_than: 0 }
   validates :price_per_day, presence: true, numericality: { greater_than: 0 }
 end
